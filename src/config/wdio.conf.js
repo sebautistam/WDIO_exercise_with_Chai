38e407a51dbf534da6d408978cf1c559a8f8b893
Sergio E Bautista M <sebautistam@gmail.com>
2025-11-30 17:25:25 -0500

    added headless

--- src/config/wdio.conf.js
+++ src/config/wdio.conf.js
@@ -52,9 +52,15 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu', '--window-size=1920,1080']
+        }
     }, {
-        browserName: 'firefox'
+        browserName: 'firefox',
+        'moz:firefoxOptions': {
+            args: ['-headless']
+        }
     }],
 
     
